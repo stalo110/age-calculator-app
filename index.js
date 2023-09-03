4884a2a26f1d7b605c7b685f3ed1503475b9a031
stanley <offorstanley4real@gmail.com>
2023-09-03 08:46:04 -0700

    Canges made

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const day = document.querySelector('#dte');
-console.log(day);
 const month = document.querySelector('#mnth');
 const year = document.querySelector('#yrs');
 const button = document.querySelector('#btn');
-
-// let isValid = false;
 
 const dayOutput = document.querySelector('.purple3');
 const monthOutput = document.querySelector('.purple2');
@@ -21,43 +18,44 @@ const inputYearError = 'Must Be a Valid Year';
 
 function checkDay() {
     let value = day.value;
-    if(value == ''){
+    let monthValue = parseInt(month.value);
+
+    if (value === '') {
         dayError.innerHTML = inputRequiredError;
         return false;
-    } else if(value < 1 || value > 31){
+    } else if (value < 1 || value > 31) {
         dayError.innerHTML = inputDayError;
         return false;
-    } else{
+    } else if ((monthValue === 4 || monthValue === 6 || monthValue === 9 || monthValue === 11) && value > 30) {
+        dayError.innerHTML = inputDayError;
+        return false;
+    } else {
         dayError.innerHTML = '';
         return true;
     }
-
 }
 
 function checkMonth() {
     let value = month.value;
-    if(value == ''){
+    if (value === '') {
         monthError.innerHTML = inputRequiredError;
         return false;
-    } else if(value < 1 || value > 12){
+    } else if (value < 1 || value > 12) {
         monthError.innerHTML = inputMonthError;
         return false;
-    } else{
+    } else {
         monthError.innerHTML = '';
         return true;
     }
-
 }
 
 function checkYear() {
     let value = year.value;
-    console.log(value);
     let currentYear = new Date().getFullYear();
-    console.log(currentYear);
-    if(value == ''){
+    if (value === '') {
         yearError.innerHTML = inputRequiredError;
         return false;
-    }else if(value > currentYear || value < 1900){
+    } else if (value > currentYear || value < 1900) {
         yearError.innerHTML = inputYearError;
         return false;
     } else {
@@ -66,29 +64,24 @@ function checkYear() {
     }
 }
 
-function calculate(birthday){
+function calculate(birthday) {
     let birthdate = new Date(birthday);
     let today = new Date();
-    console.log(birthday);
-
+    
     let years = today.getFullYear() - birthdate.getFullYear();
     let months = today.getMonth() - birthdate.getMonth();
     let days = today.getDate() - birthdate.getDate();
-    console.log(birthdate);
-    console.log(today);
-    console.log(days);
-    console.log(years);
 
-    if(months < 0 || (months === 0 & days < 0)) {
-         years = '--';
-        if (months === 0){
+    if (months < 0 || (months === 0 && days < 0)) {
+        years--;
+        if (months === 0) {
             months = 11;
-        }else{
+        } else {
             months = 12 + months;
         }
         days = 30 + days;
     }
-    console.log(years);
+
     yearOutput.innerHTML = years;
     monthOutput.innerHTML = months;
     dayOutput.innerHTML = days;
@@ -101,23 +94,8 @@ button.addEventListener('click', e => {
     let inputMonth = checkMonth();
     let inputYear = checkYear();
 
-    console.log(inputDay);
+    if (!inputDay || !inputMonth || !inputYear) return;
 
-    if(!inputDay||!inputMonth||!inputYear) return
-    let birthday = `${day.value}/${month.value}/${year.value}`;
+    let birthday = `${year.value}-${month.value}-${day.value}`;
     calculate(birthday);
-    console.log(calculate);
 });
-
-
-// day.addEventListener('submit', e =>{
-// if (+day.value>31) {
-//     // document.getElementById('dayError').style.display = 'block';
-//     dayError.textContent = 'Must Be A Valid Date';
-//     isValid = false;
-//     return;
-// } else if(){
-    
-// }
-
-// });
